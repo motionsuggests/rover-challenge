c683aedc0ecd74881f30329802b7ebabd75b4e17
Jonathan Taylor <jonathan.taylor@motionsuggests.com>
2018-03-16 13:17:45 -0400

    started sequencer, planetgrid

---
+++ src/components/PlanetGrid/PlanetGrid.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './PlanetGrid.css';
+
+class PlanetGrid extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="grid">
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+            </div>
+        );
+    }
+}
+
+PlanetGrid.propTypes = {
+
+};
+
+export default PlanetGrid;
---
+++ src/components/Sequencer/Sequencer.js
@@ -0,0 +1,19 @@
+class Sequencer {
+    
+    constructor(sequence) {
+
+        this.sequence = sequence;
+        this.currentStep = 0;
+    }
+    
+    // Used to set start cardinalDirection on app init
+    start(cardinalDirection, compassArr) {
+        // takes cardinalDirection and compassArr of [n, e, s, w]
+        // returns compassArr with cardinalDirection at index 0
+    }
+
+    
+    
+  }
+
+export { Sequencer as default}
