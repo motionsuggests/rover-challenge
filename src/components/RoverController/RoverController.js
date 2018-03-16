cdb27d8697a94d5a3237be5c3ebee9d17b916aad
Jonathan Taylor <jonathan.taylor@motionsuggests.com>
2018-03-16 17:26:55 -0400

    started to fill some functions

--- src/components/RoverController/RoverController.js
+++ src/components/RoverController/RoverController.js
@@ -6,6 +6,13 @@ class RoverControler {
     static setCardinalDirection (cardinalDirection, compassArr) {
         // takes cardinalDirection and compassArr of [n, e, s, w]
         // returns compassArr with cardinalDirection at index 0
+        let _compassArr = compassArr.splice();
+        console.log(_compassArr)
+        // while(_compassArr[0] !== cardinalDirection) {
+        //     let cardinal = compassArr.pop()
+        //     // _compassArr = _compassArr.unshift(cardinal);
+        // }
+        return _compassArr;
     }
 
     static turnRover(direction, compassArr) {
