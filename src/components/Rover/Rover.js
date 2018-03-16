93b8b45f0982fd492ee399dcefb60e03ca6b7a6c
Jonathan Taylor <jonathan.taylor@motionsuggests.com>
2018-03-16 16:32:03 -0400

    updated rover to handle rotation

--- src/components/Rover/Rover.js
+++ src/components/Rover/Rover.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 import './Rover.css';
 
+const rotation = {
+    n: '0deg',
+    e: '90deg',
+    s: '180deg',
+    w: '-90deg'
+} 
+
 class Rover extends Component {
 
     render() {
+        const style = {transform: `rotate( ${rotation[this.props.cardnialDirection]})`}
         return (
-            <div className="rover"/>
+            <div className="rover" style={style}/>
         );
     }
 }
